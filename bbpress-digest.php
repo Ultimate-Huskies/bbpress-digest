542295970eefcac9d16d9ece5824c6e6745108ea
Milan Dinic <milan@srpski.biz>
2012-07-21 16:24:10 +0000

    Bump version to beta.

--- bbpress-digest.php
+++ bbpress-digest.php
@@ -15,7 +15,7 @@
  * Description: Send digests with forum's active topics.
  * Author:      Milan Dinić
  * Author URI:  http://blog.milandinic.com/
- * Version:     2.0-alfa-3
+ * Version:     2.0-beta-1
  * Text Domain: bbp-digest
  * Domain Path: /languages/
  * License:     GPL
