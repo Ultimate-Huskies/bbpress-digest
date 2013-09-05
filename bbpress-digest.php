ffdd4935b1c33ca638d9b8f55a3dc159e66be255
Milan Dinic <milan@srpski.biz>
2013-09-05 23:16:40 +0000

    Bump version number. At this point all issues are fixed and we're compatible with bbPress 2.4 and WordPress 3.6.

--- bbpress-digest.php
+++ bbpress-digest.php
@@ -15,7 +15,7 @@
  * Description: Send digests with forum's active topics.
  * Author:      Milan Dinić
  * Author URI:  http://blog.milandinic.com/
- * Version:     2.1-alpha-1
+ * Version:     2.1-alpha-2
  * Text Domain: bbp-digest
  * Domain Path: /languages/
  * License:     GPL
